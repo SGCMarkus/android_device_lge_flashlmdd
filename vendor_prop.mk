abcd65ace6b797b24d7facce3b0b7d073e381a4c
SGCMarkus <markusornik@gmail.com>
2020-03-14 14:53:32 +0100

    flashlmdd: add device specific RIL props for ECC list

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -2,4 +2,11 @@
 # vendor props for flashlmdd
 #
 
-
+# LGE Misc
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.lge.build.target_country=COM \
+    ro.vendor.lge.build.target_operator=LAO \
+    ro.vendor.lge.capp_cupss.op.dir=/product/OP \
+    ro.vendor.lge.capp_cupss.persistlg.dir=/mnt/vendor/persist-lg \
+    ro.vendor.lge.capp_cupss.rootdir=/product/OP \
+    ro.vendor.lge.swversion=V50020e
