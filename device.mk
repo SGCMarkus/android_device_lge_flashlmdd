3f82c03991c62da309af933f9832f99e66ec374f
SGCMarkus <markusornik@gmail.com>
2021-10-22 23:15:12 +0200

    flashlmdd: device device name
    define device name so we can use it in sm8150-common to copy fstab/init files properly

--- device.mk
+++ device.mk
@@ -20,6 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product-if-exists, vendor/lge/flashlmdd/flashlmdd-vendor.mk)
 
 DEVICE_PATH := device/lge/flashlmdd
+DEVICE_NAME := flashlmdd
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
